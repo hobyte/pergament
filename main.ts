dd7466c9ed4fff0f4de6d02dbbbeee65005ade84
hobyte <simolu8@gmail.com>
2024-01-20 21:58:56 -0800

    add save command to manipulate pergament codeblock content

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EditorView } from '@codemirror/view';
 
 // Remember to rename these classes and interfaces!
 
@@ -15,67 +16,57 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		console.log("loaded settings");
+		const id = "newid";
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.registerMarkdownCodeBlockProcessor("pergament", (source, el, ctx) => {
+			console.log("render code block");
+			const mdView: MarkdownView | null = this.app.workspace.getActiveViewOfType(MarkdownView)
+
+			if (mdView?.getMode() === 'preview') {
+				console.log("preview mode, just render text");
+				el.innerHTML = source;
+				return;
+			}
+
+			console.log("other mode, trying to change content");
+
+			const mark = el.createEl('div');
+			mark.setAttr("id", id);
+			mark.innerHTML = "this is a pergament codeblock";
+			el.appendChild(mark);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		//accessing the editor content only works in a function sepereate from rendering the markdown codeblock
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "save",
+			name: "save content to md file",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				console.log("save mode");
+				const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (mdView.getMode() === 'preview') return;
+
+				const view = mdView.editor.cm as EditorView;
+				const mark = document.getElementById(id);
+
+				const pos = view.posAtDOM(mark as Node);
+				var line = view.state.doc.lineAt(pos);
+				if (line.text.contains('```pergament')) {
+					line = view.state.doc.lineAt(line.to + 1);
 				}
-			}
+				//line contains the position of the codeblock content
+				console.log(pos, line);
+
+				//save new content to codeblock
+				const now = new Date();
+				const text = line.text;
+
+				let transaction = view.state.update({changes: {from: line.from, to: line.to, insert: now.toLocaleString()}})
+				view.dispatch(transaction);
+				console.log("updated codeblock");
+			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,21 +82,6 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
