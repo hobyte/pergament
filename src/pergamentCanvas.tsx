69056097fcf302e50066f8ecde270f707712c2df
hobyte <simolu8@gmail.com>
2024-01-21 20:48:34 -0800

    install kova canvas library

--- src/pergamentCanvas.tsx
+++ src/pergamentCanvas.tsx
@@ -1,3 +1,12 @@
+import { Circle, Layer, Rect, Stage } from "react-konva";
+
 export const PergamentCanvas = () => {
-    return <h4>Hello, React!</h4>;
+    return (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+                <Rect width={50} height={50} fill="red" />
+                <Circle x={200} y={200} stroke="black" radius={50} />
+            </Layer>
+        </Stage>
+    );
 };
