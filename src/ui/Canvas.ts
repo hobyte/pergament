32aa697e869f838f91e71354c65767bee63a281b
hobyte <simolu8@gmail.com>
2024-03-09 20:29:25 -0800

    draw line with mouse

--- src/ui/Canvas.ts
+++ src/ui/Canvas.ts
@@ -1,4 +1,5 @@
 import { Layer } from "konva/lib/Layer";
+import { KonvaEventListener } from "konva/lib/Node";
 import { Stage } from "konva/lib/Stage";
 import { Line } from "konva/lib/shapes/Line";
 import { BackgroundPattern } from "src/settings/Background";
@@ -9,6 +10,10 @@ export class Canvas {
     private parent: HTMLElement
     private stage: Stage
     private backgroundLayer: Layer
+    private drawingLayer: Layer
+
+    private drawing = false
+    private currentLine: Line
 
     constructor(parent: HTMLElement, settings: Settings) {
         this.settings = settings;
@@ -17,9 +22,16 @@ export class Canvas {
         this.watchParentResize();
 
         //draw background
-        this.backgroundLayer = new Layer({name: 'background'});
+        this.backgroundLayer = new Layer({ name: 'background' });
         this.stage.add(this.backgroundLayer);
         this.drawBackground();
+
+        //event listeners and layer for tools
+        this.drawingLayer = new Layer({name: 'draw'});
+        this.stage.add(this.drawingLayer)
+        this.stage.on('mousedown touchstart', event => this.startTool(event));
+        this.stage.on('mousemove touchmove', event => this.moveTool(event));
+        this.stage.on('mouseup touchend', event => this.endTool(event));
     }
 
     private createStage(parent: HTMLElement): void {
@@ -75,17 +87,54 @@ export class Canvas {
                         name: 'vertical'
                     }))
                 }
-                //no break to run next case and draw lines of grid
+            //no break to run next case and draw lines of grid
             case BackgroundPattern.line:
                 //draw horizontal lines
                 for (let y = 0; y < stageHeight; y += this.settings.background.size) {
                     this.backgroundLayer.add(new Line({
                         stroke: this.settings.background.color,
-                        points: [0,y, stageWidth, y],
+                        points: [0, y, stageWidth, y],
                         name: 'horizontal'
                     }))
                 }
                 break;
         }
+    }
+
+    private startTool(event) {
+        const pos = this.stage.getPointerPosition();
+        //check of pos is null
+        if (!pos) {
+            console.log('failed to get cursor position')
+            return
+        }
+        
+        this.drawing = true;
+        this.currentLine = new Line({
+            stroke: '#F74F23',
+            strokeWidth: 5,
+            lineCap: 'round',
+            lineJoin: 'round',
+            points: [pos.x, pos.y, pos.x, pos.y]
+        })
+        this.drawingLayer.add(this.currentLine);
+    }
+
+    private moveTool(event) {
+        if (!this.drawing) {
+            return;
+        }
+
+        const pos = this.stage.getPointerPosition();
+        //check of pos is null
+        if (!pos) {
+            console.log('failed to get cursor position')
+            return
+        }
+        this.currentLine.points(this.currentLine.points().concat([pos.x, pos.y]))
+    }
+
+    private endTool(event) {
+        this.drawing = false;
     }
 }
