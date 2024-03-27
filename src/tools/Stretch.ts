5a8c112d5d648c328a8dcf770717f47f9ab256ee
hobyte <simolu8@gmail.com>
2024-03-26 20:22:48 -0700

    move all canvas conetent below cursor up and down

--- src/tools/Stretch.ts
+++ src/tools/Stretch.ts
@@ -1,16 +1,19 @@
 import { Layer } from "konva/lib/Layer";
 import { Tool } from "./Tool";
 import { Line } from "konva/lib/shapes/Line";
+import { Shape } from "konva/lib/Shape";
 
-export class Stretch extends Tool{
+export class Stretch extends Tool {
     private movingLine: Line
     private moving: boolean
+    private firstPos: { x: number, y: number }
+    private oldPos: { x: number, y: number }
     private toolName = 'stretch-tool'
 
-    constructor(name: string, removable: boolean){
+    constructor(name: string, removable: boolean) {
         super(name, removable);
     }
-    
+
     public start(layer: Layer): void {
         const pos = layer.getRelativePointerPosition();
         this.movingLine = new Line({
@@ -18,24 +21,38 @@ export class Stretch extends Tool{
             width: 3,
             stroke: '#A835DB'
         })
-        
-        if(pos){
+
+        if (pos) {
             this.moving = true;
+            this.firstPos = pos;
+            this.oldPos = pos;
 
             layer.add(this.movingLine);
-            this.movingLine.points([0, pos.y, layer.width(), pos.y])
+            this.movingLine.points([0, pos.y, layer.width(), pos.y]);
         }
     }
-    
+
     public move(layer: Layer): void {
-        if(this.moving){
+        if (this.moving) {
             const pos = layer.getRelativePointerPosition();
-            if(pos){
+            if (pos) {
                 this.movingLine.points([0, pos.y, layer.width(), pos.y])
+
+                //move vanvas content below line
+                const deltaY = pos.y - this.oldPos.y;
+                this.oldPos = pos;
+
+                layer
+                    .find((node: Shape) => {
+                        const dimension = node.getClientRect();
+                        return dimension.y + dimension.height >= pos.y
+                    })
+                    .filter((node: Shape) => node.name() != this.toolName)
+                    .forEach((node: Shape) => node.move({x: 0, y: deltaY}));
             }
         }
     }
-    
+
     public end(layer: Layer): void {
         this.moving = false;
         this.movingLine.remove();
